6c2445652b4eade3a0207f6b6ef479bb2f2700f7
Devjo3 <138790915+Devjo3@users.noreply.github.com>
2024-02-03 02:30:19 -0300

    FrontEnd
    desenvolvimento e montagem da tela principal

---
+++ rgp/assets/js/scripts.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function () {
+    // Fecha o menu ao clicar em um item
+    $('.navbar-nav a').on('click', function () {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+    // Fecha o menu ao clicar no botão de toggle
+    $('#navbarSideCollapse').on('click', function () {
+      $('.navbar-collapse').collapse('hide');
+    });
+  });
+
